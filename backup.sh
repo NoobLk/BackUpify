d5ef18a55829c5c216d97333590917ba68ba026d
weblankanteam <support@weblankan.com>
2025-03-06 09:44:10 +0530

    update restore script

--- backup.sh
+++ backup.sh
@@ -7,8 +7,8 @@ set +a
 
 # Get current timestamp
 timestamp=$(date +%Y_%m_%d_%H:%M:%S)
-backup_path="./backups/$SITE_NAME/$timestamp"
-backup_path2="./backups/$SITE_NAME/"
+backup_dir="./backups/$SITE_NAME/"
+
 
 function run_backup() {
     mkdir -p "$backup_path"
@@ -16,21 +16,23 @@ function run_backup() {
     Uncomment below to enable database backup
     echo "Backing up database..."
     if [[ "$INCLUDE_DB" == "yes" ]]; then
-        mysqldump -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$backup_path/db.sql"
+
+    # Backup MySQL database
+        mysqldump -h "$DB_HOST" -P "$DB_PORT" --no-tablespaces -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$backup_dir/db.sql"
     else
         echo "Database backup is disabled for this configuration."
     fi
 
     echo "Backing up $SITE_NAME files..."
     if [ -d "$WP_DIR" ]; then
-        tar -czf "$backup_path/wp_files.tar.gz" -C "$WP_DIR" .
+        tar -czf "$backup_dir/$timestamp/wp_files.tar.gz" -C "$WP_DIR" .
         echo "File backup successful."
     else
         echo "Warning: The WordPress directory does not exist: $WP_DIR"
     fi
 
 
-    echo "Backup for $SITE_NAME completed at $backup_path"
+    echo "Backup for $SITE_NAME completed at $backup_dir"
 
     # Remove old backups if the number exceeds MAX_BACKUP_COUNT
 
@@ -39,13 +41,42 @@ function run_backup() {
 
 # Function to clean up old backups
 cleanup_backups() {
-    BACKUPS=($(ls -dt "$backup_path2"/*))
+    BACKUPS=($(ls -dt "$backup_dir"/*))
     if [ ${#BACKUPS[@]} -gt $MAX_BACKUPS ]; then
         for ((i = $MAX_BACKUPS; i < ${#BACKUPS[@]}; i++)); do
             echo "Deleting old backup: ${BACKUPS[$i]}"
             rm -rf "${BACKUPS[$i]}"
         done
     fi
+}
+
+
+
+restore_backup() {
+    if [ -z "$1" ]; then
+        echo "Usage: $0 restore <YYYY-MM-DD_HH-MM-SS>"
+        exit 1
+    fi
+
+    BACKUP_PATH="$BACKUP_DIR/backup_$1"
+
+    if [ ! -d "$BACKUP_PATH" ]; then
+        echo "Backup not found: $BACKUP_PATH"
+        exit 1
+    fi
+
+    echo "Restoring backup from $BACKUP_PATH..."
+
+    # Restore WordPress files
+    tar -xzf "$BACKUP_PATH/wordpress_files.tar.gz" -C "$WORDPRESS_DIR" --strip-components=1
+
+    # Drop and recreate the database
+    docker exec -i $DB_CONTAINER mysql -u$DB_USER -p$DB_PASSWORD -e "DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_NAME;"
+
+    # Restore MySQL database
+    docker exec -i $DB_CONTAINER mysql -u$DB_USER -p$DB_PASSWORD $DB_NAME < "$BACKUP_PATH/db_backup.sql"
+
+    echo "Restore completed."
 }
 
 
